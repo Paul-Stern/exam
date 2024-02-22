cb33129bd6f2bc019c683f0c7641772a10460136
Paul Stern <paulstern45@gmail.com>
2024-02-22 14:32:41 +0300

    Implement (only) basic functionality
    Implement web server, html template, form handling.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module heywood/webtest
+
+go 1.21.1
---
+++ webtest.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Card struct {
+	Question string
+	Options  []string
+}
+
+var testCard = Card{
+	Question: "Что есть оториноларинголог?",
+	Options:  []string{"Ухо-горло-нос", "Печень-желчь-кишка", "Глаза-язык-легкие"},
+}
+
+var templates = template.Must(template.ParseFiles("card.html"))
+
+func main() {
+	// fmt.Println(testCard)
+	http.HandleFunc("/", makeHandler(viewHandler))
+
+	log.Fatal(http.ListenAndServe(":***REMOVED***", nil))
+}
+
+func loadCard(c Card) *Card {
+	return &c
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, c Card) {
+	// data := loadCard(c)
+	renderTemplate(w, "card", &c)
+	r.ParseForm()
+	vals := r.Form
+	// v := r.FormValue("body")
+	log.Println(vals)
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, Card)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fn(w, r, testCard)
+	}
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, c *Card) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", c)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
