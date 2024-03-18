01a98aa799fada6533c155b0c114e3a01249e256
Paul Stern <paulstern45@gmail.com>
2024-03-18 19:48:21 +0300

    QSTN-69 Add service functionality. Install and run the service

--- go.mod
+++ go.mod
@@ -4,5 +4,8 @@ go 1.21.1
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/kardianos/service v1.2.2
 	gopkg.in/yaml.v3 v3.0.1
 )
+
+require golang.org/x/sys v0.0.0-20201015000850-e3ed0017c211 // indirect
--- main.go
+++ main.go
@@ -4,21 +4,23 @@ import (
 	_ "embed"
 	"log"
 	"net/http"
+
+	"github.com/kardianos/service"
 )
 
 // Used to store value from LDFLAGS
 var version string
 
-func main() {
-	readConf(&cfg)
-	err := LoadTemplates()
-	if err != nil {
-		log.Fatalf("LoadTemplates error: %v", err)
-	}
-	if version == "" {
-		version = "dev"
-	}
+var logger service.Logger
 
+type program struct{}
+
+func (p *program) Start(s service.Service) error {
+	// Start should not block. Do the actual work async.
+	go p.run()
+	return nil
+}
+func (p *program) run() {
 	// cert(255)
 	http.HandleFunc("/login", signInHandler)
 	http.HandleFunc("/signup", signUpHandler)
@@ -32,4 +34,50 @@ func main() {
 	// paths to the cert and the key
 	log.Printf("Server started. Listening to %s:%s", cfg.Server.Addr, cfg.Server.Port)
 	log.Fatal(http.ListenAndServeTLS(cfg.Server.Addr+":"+cfg.Server.Port, cfg.Server.Cert, cfg.Server.Key, nil))
+}
+func (p *program) Stop(s service.Service) error {
+	// Stop should not block. Return with a few seconds.
+	log.Println("Server stopped")
+	return nil
+}
+
+func main() {
+	readConf(&cfg)
+	err := LoadTemplates()
+	if err != nil {
+		log.Fatalf("LoadTemplates error: %v", err)
+	}
+	if version == "" {
+		version = "dev"
+	}
+	svcConfig := &service.Config{
+		Name:        "WebtestService",
+		DisplayName: "Webtest Service",
+		Description: "Веб-сервер системы тестирования",
+		Option: service.KeyValue{
+			"UserService": true,
+			"Interactive": true,
+		},
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logger, err = s.Logger(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = s.Install()
+	if err != nil {
+		logger.Error(err)
+	} else {
+		log.Println("Service installed")
+	}
+	err = s.Run()
+	if err != nil {
+		logger.Error(err)
+	}
+
 }
